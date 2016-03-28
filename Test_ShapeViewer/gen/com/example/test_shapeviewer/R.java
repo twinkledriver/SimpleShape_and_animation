a94d8027a3a704916c6303cf0abb7121141a1896
Gao Shengxu <1059067158@qq.com>
2016-03-28 21:28:01 +0800

    Processing..

--- Test_ShapeViewer/gen/com/example/test_shapeviewer/R.java
+++ Test_ShapeViewer/gen/com/example/test_shapeviewer/R.java
@@ -11,10 +11,23 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int green_rect=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int menu_settings=0x7f070000;
+        public static final int ButtonArc=0x7f070005;
+        public static final int ButtonLine=0x7f070001;
+        public static final int ButtonOval=0x7f070002;
+        public static final int ButtonPath=0x7f070004;
+        public static final int ButtonPath2=0x7f070008;
+        public static final int ButtonPath3=0x7f070009;
+        public static final int ButtonRect=0x7f070003;
+        public static final int ButtonRoundRect=0x7f070006;
+        public static final int ButtonRoundRect2=0x7f070007;
+        public static final int ButtonRow=0x7f070000;
+        public static final int ImageViewForShape=0x7f07000b;
+        public static final int LinearLayout01=0x7f07000a;
+        public static final int menu_settings=0x7f07000c;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -25,7 +38,7 @@ public final class R {
     public static final class string {
         public static final int app_name=0x7f040000;
         public static final int hello_world=0x7f040001;
-        public static final int menu_settings=0x7f040002;
+        public static final int shapes=0x7f040002;
     }
     public static final class style {
         /** 
--- Test_ShapeViewer/src/com/example/test_shapeviewer/MainActivity.java
+++ Test_ShapeViewer/src/com/example/test_shapeviewer/MainActivity.java
@@ -3,21 +3,27 @@ package com.example.test_shapeviewer;
 
 
 
-import com.androidbook.simpleshapes.R;
 
 import android.app.Activity;
 import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
 import android.graphics.RectF;
+import android.graphics.drawable.Drawable;
 import android.graphics.drawable.ShapeDrawable;
+import android.graphics.drawable.shapes.ArcShape;
+import android.graphics.drawable.shapes.OvalShape;
+import android.graphics.drawable.shapes.PathShape;
 import android.graphics.drawable.shapes.RectShape;
 import android.graphics.drawable.shapes.RoundRectShape;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends Activity {
 
-    @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
@@ -47,15 +53,13 @@ public class MainActivity extends Activity {
 				d.setIntrinsicWidth(100);
 				d.getPaint().setColor(Color.RED);
 				setShapeByDrawable(d);
-
-		};
+			}
+		});
       
         
-        final Button rectButton = (Button) findViewById(R.id.ButtonRect);
-        rectButton.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View arg0) {
-				// TODO Auto-generated method stub
+		final Button rectButton = (Button) findViewById(R.id.ButtonRect);
+		rectButton.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
 				setShapeByResourceId(R.drawable.green_rect);
 			}
 		});
@@ -93,26 +97,108 @@ public class MainActivity extends Activity {
      	           setShapeByDrawable(d);
    			}
    		});
-     			}
-     				
-     			}
-        
-        
-        
-        
-        
-        
-    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.activity_main, menu);
-        return true;
-    }
-    
-    
-    
-    
+	final Button pathButton=(Button) findViewById(R.id.ButtonPath);
+	pathButton.setOnClickListener(new View.OnClickListener() {
+		
+		@Override
+		public void onClick(View v) {
+			// TODO Auto-generated method stub
+			
+			Path p = new Path(); // pretty star, filled
+			p.moveTo(50, 0);
+			p.lineTo(25,100);
+			p.lineTo(100,50);
+			p.lineTo(0,50);
+			p.lineTo(75,100);
+			p.lineTo(50,0); 
+			
+			ShapeDrawable d = new ShapeDrawable(new PathShape(p, 100, 100));
+			d.setIntrinsicHeight(100);
+			d.setIntrinsicWidth(100);
+			d.getPaint().setColor(Color.YELLOW);
+			setShapeByDrawable(d);
+	
+		}
+	});
+	
+		final Button path2button=(Button) findViewById(R.id.ButtonPath2);
+		path2button.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				
+				Path p = new Path();
+				
+				p.setFillType(Path.FillType.EVEN_ODD); // EVEN_ODD 意味着如果用一条直线横贯图形时，外部和内部交替出现。另外还有效果：WINDING 
+				p.moveTo(50, 0);
+				p.lineTo(25,100);
+				p.lineTo(100,50);
+				p.lineTo(0,50);
+				p.lineTo(75,100);
+				p.lineTo(50,0);  
+				
+				ShapeDrawable d = new ShapeDrawable(new PathShape(p, 100, 100));
+				d.setIntrinsicHeight(100);
+				d.setIntrinsicWidth(100);
+				d.getPaint().setColor(Color.YELLOW);
+				setShapeByDrawable(d);
+			}
+		});
+     		
+     	final Button path3button=(Button) findViewById(R.id.ButtonPath3);
+     	path3button.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				
+				Path p = new Path();
+				p.moveTo(50, 0);
+				p.lineTo(25,100);
+				p.lineTo(100,50);
+				p.lineTo(0,50);
+				p.lineTo(75,100);
+				p.lineTo(50,0);  
+				
+				ShapeDrawable d = new ShapeDrawable(new PathShape(p, 100, 100));
+				d.setIntrinsicHeight(100);
+				d.setIntrinsicWidth(100);
+				d.getPaint().setColor(Color.YELLOW);
+				d.getPaint().setStyle(Paint.Style.STROKE); // Line drawing
+				
+				setShapeByDrawable(d);
+	
+			}
+		});
+     	
+    	final Button arcButton = (Button) findViewById(R.id.ButtonArc);
+		arcButton.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				ShapeDrawable d = new ShapeDrawable(new ArcShape(0, 345));
+				d.setIntrinsicHeight(100);
+				d.setIntrinsicWidth(100);
+				d.getPaint().setColor(Color.MAGENTA);
+				setShapeByDrawable(d);
+			}
+		});
+		
 }
-}
+ 
+        
+        private void setShapeByResourceId(int resourceId) {
+    		// We will animate the imageview
+    		ImageView reusableImageView = (ImageView) findViewById(R.id.ImageViewForShape);
+    		reusableImageView.setImageResource(resourceId);
+    	}
+        
+    	private void setShapeByDrawable(Drawable drawable) {
+    		// We will animate the imageview
+    		ImageView reusableImageView = (ImageView) findViewById(R.id.ImageViewForShape);
+    		reusableImageView.setImageDrawable(drawable);
+    	}
+  }
+
+
+
